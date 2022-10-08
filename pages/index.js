9533a880368c49bc57f9a170b00b4879e9e507a2
narendra-sajwani <niks.imagination@gmail.com>
2022-10-08 12:57:48 +0530

    written moralis cloud functions, added events to moralis database

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import { useMoralisQuery } from "react-moralis"
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            Hi!
-        </div>
+    // How do we show the recenly listed NFTs?
+
+    // We will index the events off chain and then read from our databse.
+    // Setup a server to listen for those events to be fired,
+    // and we will add them to database to query.
+
+    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+        // Table name
+        // Function for the query
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
     )
+    console.log(listedNfts)
+    return <div className={styles.container}>Hi!</div>
 }
