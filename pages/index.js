104b770188eedccc5a832eac501a2275328a362c
narendra-sajwani <niks.imagination@gmail.com>
2022-10-09 20:57:53 +0530

    completed NFTBox component and created UpdateListingModal component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import { useMoralisQuery } from "react-moralis"
+import { useMoralisQuery, useMoralis } from "react-moralis"
 import NFTBox from "../components/NFTBox"
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
     // We will index the events off chain and then read from our databse.
     // Setup a server to listen for those events to be fired,
     // and we will add them to database to query.
-
+    const { isWeb3Enabled } = useMoralis()
     const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
         // Table name
         // Function for the query
@@ -18,30 +18,35 @@ export default function Home() {
     )
     console.log(listedNfts)
     return (
-        <div className={styles.container}>
-            {fetchingListedNfts ? (
-                <div>Loading...</div>
-            ) : (
-                listedNfts.map((nft) => {
-                    console.log(nft.attributes)
-                    const { price, nftAddress, tokenId, marketplaceAddress, seller } =
-                        nft.attributes
-                    return (
-                        <div>
-                            Price: {price}. NftAddress: {nftAddress}. TokenId: {tokenId}. Seller:
-                            {seller}
-                            <NFTBox
-                                price={price}
-                                nftAddress={nftAddress}
-                                tokenId={tokenId}
-                                marketplaceAddress={marketplaceAddress}
-                                seller={seller}
-                                key={`${nftAddress}${tokenId}`}
-                            />
-                        </div>
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    fetchingListedNfts ? (
+                        <div>Loading...</div>
+                    ) : (
+                        listedNfts.map((nft) => {
+                            console.log(nft.attributes)
+                            const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                                nft.attributes
+                            return (
+                                <div>
+                                    <NFTBox
+                                        price={price}
+                                        nftAddress={nftAddress}
+                                        tokenId={tokenId}
+                                        marketplaceAddress={marketplaceAddress}
+                                        seller={seller}
+                                        key={`${nftAddress}${tokenId}`}
+                                    />
+                                </div>
+                            )
+                        })
                     )
-                })
-            )}
+                ) : (
+                    <div>Web3 Currently Not Enabled</div>
+                )}
+            </div>
         </div>
     )
 }
